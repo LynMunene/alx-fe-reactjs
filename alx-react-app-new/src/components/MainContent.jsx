1c38fcf20f57048fe6fa1ca164af394c29d2c02c
LynMunene <157380414+LynMunene@users.noreply.github.com>
2025-07-20 20:29:09 +0300

    Added a copy of react app with inline styling

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+function MainContent (){
+    return(
+     <main>
+        <p style={{ color:'purple', fontFamily:'poppins',  padding:'10px'}}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+
+    )
+
+}
+export default MainContent
