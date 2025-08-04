0e48172ecea01c07c78d1e2cf468851db60754de
LynMunene <157380414+LynMunene@users.noreply.github.com>
2025-08-04 19:54:31 +0300

    Setup routing in app.js

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,9 +1,12 @@
-
-
-function App() {
-
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Home from './components/Home'
+function App (){
   return (
-   
+    <Router>
+      <Routes>
+        <Route path = "/" element ={<Home />} />
+      </Routes>
+    </Router>
   )
 }
 
