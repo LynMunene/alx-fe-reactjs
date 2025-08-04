226ac975d69d8f354de8066850e8d25797d74bec
LynMunene <157380414+LynMunene@users.noreply.github.com>
2025-08-04 12:45:12 +0300

    Created a user-search-application

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -4,7 +4,7 @@ import RecipeDetails from './components/RecipeDetails';
 import EditRecipeForm from './components/EditRecipeForm';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBar from './components/SearchBar';
-import FavoritesList from './components/FavouritesList';
+import FavoritesList from './components/FavoritesList';
 import RecommendationsList from './components/RecommendationsList';
 
 function App() {
